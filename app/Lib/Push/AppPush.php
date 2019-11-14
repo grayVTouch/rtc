0c46e57158ee497051a07ec43e255217f87c074b
grayVTouch <A576236148946@126.com>
2019-11-14 09:46:03 +0800

    by cxl

--- app/Lib/Push/AppPush.php
+++ app/Lib/Push/AppPush.php
@@ -63,7 +63,9 @@ class AppPush {
         $data['content'] = $content;
         $data['title'] = $title;
 //        $data['extra'] = $extra;
-        $data['extra'] = json_encode(json_encode(['name' => 'running']));
+//        $data['extra'] = json_encode(json_encode(['name' => 'running']));
+//        $data['extra'] = $extra;
+        $data['extra'] = ['name' => 'running'];
         $res = self::curl('/push' , $data);
         if (empty($res)) {
             return self::response('请求发送失败，请检查网络' , 500);
