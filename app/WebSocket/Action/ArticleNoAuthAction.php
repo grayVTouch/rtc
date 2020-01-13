2f87fb5c8b9c7c150bb77783a0eed7c4eb1ff321
grayVTouch <A576236148946@126.com>
2020-01-13 17:52:14 +0800

    by cxl branch qianwangV1 代码更新

---
+++ app/WebSocket/Action/ArticleNoAuthAction.php
@@ -0,0 +1,42 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: grayVTouch
+ * Date: 2019/10/31
+ * Time: 15:31
+ */
+
+namespace App\WebSocket\Action;
+
+
+use App\Model\ArticleModel;
+use App\Util\PageUtil;
+use App\WebSocket\Auth;
+use Core\Lib\Validator;
+
+class ArticleNoAuthAction extends Action
+{
+
+    public static function firstByArticleTypeId(Auth $auth , int $article_type_id , array $param)
+    {
+        $res = ArticleModel::firstByArticleTypeId($article_type_id);
+        return self::success($res);
+    }
+
+    public static function detailForArticle(Auth $auth , int $article_type_id , array $param)
+    {
+        $validator = Validator::make($param , [
+            'id' => 'required' ,
+        ]);
+        if ($validator->fails()) {
+            return self::error($validator->message());
+        }
+        $res = ArticleModel::findByIdAndArticleTypeId($param['id'] , $article_type_id);
+        if (empty($res)) {
+            return self::error('未找到对应的文章' , 404);
+        }
+        return self::success($res);
+    }
+
+
+}
