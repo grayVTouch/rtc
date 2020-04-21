fb0a425996f648ec56b9618a11acbb58b7f4823c
grayVTouch <A576236148946@126.com>
2020-04-21 14:16:46 +0800

    by cxl

--- app/Http/ApiV1/Controller/Login.php
+++ app/Http/ApiV1/Controller/Login.php
@@ -17,6 +17,9 @@ class Login extends Base
     public function login()
     {
         $param = $this->request->post;
+        $param['unique_code'] = $param['unique_code'] ?? '';
+        $param['token'] = $param['token'] ?? '';
+        $param['platform'] = $param['platform'] ?? '';
         $res = LoginAction::login($this , $param);
         if ($res['code'] != 0) {
             return self::error($res['data'] , $res['code']);
