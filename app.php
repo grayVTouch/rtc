9ca0d933808076c51f46190bfc6ad5e48a789857
Tuuz <2539@aerofsx.com>
2020-05-28 17:01:00 +0800

    production

--- app.php
+++ app.php
@@ -20,7 +20,7 @@
  * 根据开发还是调试可以选择不同的环境
  * 这样就不会导致相关方面冲突
  */
-//const ENV = 'production';
-const ENV = 'development';
+const ENV = 'production';
+//const ENV = 'development';
 
 require_once __DIR__ . '/bootstrap/bootstrap.php';
