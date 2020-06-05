48c2a83011493dc524af87483eb87f3a05c31ed9
running <A576236148946@126.com>
2020-06-05 21:24:18 +0800

    by cxl

--- app/WebSocket/V1/Redis/CacheRedis.php
+++ app/WebSocket/V1/Redis/CacheRedis.php
@@ -12,10 +12,10 @@ use Engine\Facade\Redis as RedisFacade;
 
 class CacheRedis extends Redis
 {
-    public static function value(string $name , string $value = '' , int $expire = 0)
+    public static function value(string $name , string $value = '' , int $expire = 172800)
     {
         if (empty($value)) {
-            return RedisFacade::string($name , $value);
+            return RedisFacade::string($name);
         }
         return RedisFacade::string($name , $value , $expire);
     }
