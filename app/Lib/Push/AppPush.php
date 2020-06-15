7a071f0ea712d74d1db7697653d509ad53d73fda
Tuuz <2539@aerofsx.com>
2020-06-15 15:32:24 +0800

    production

--- app/Lib/Push/AppPush.php
+++ app/Lib/Push/AppPush.php
@@ -27,10 +27,10 @@ class AppPush {
     private static $api = 'http://push.t1.tuuz.cc';
 
     // nimo 推送
-    private static $token = 'nimo2';
+    private static $token = 'nesm';
 
     // 专门针对 ios 进行的推送
-    private static $tokenForIos = 'nimo2';
+    private static $tokenForIos = 'nesm';
 
     /*
      *
