3a96ba3780804d55eb2af012fb75d16c36f7c7cf
Tuuz <2539@aerofsx.com>
2020-06-18 16:11:17 +0800

    production

--- config/app.php
+++ config/app.php
@@ -156,7 +156,7 @@ return [
     // app 下载连接地址
 //    'app_download' => 'https://sapp.link/download/nxmq2' ,
 //    'app_download' => 'https://fir.im/OJBKapk' ,
-    'app_download' => 'http://download.hichatvip.com/' ,
+    'app_download' => 'http://d.firim.info/Hichat/' ,
 
     // 新用户注册推送
     'new_user_notification' => '欢迎使用嗨聊！嗨聊通信是最安全的通信软件，所有的消息和通话都是通过点对点加密传输。' ,
